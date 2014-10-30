df2534acbb3d4fb72ee6684701b26656d101b77e
Alexandre Possebom <alexandrepossebom@gmail.com>
2014-10-30 20:09:47 -0200

    This is where it all begins...

---
+++ app/src/main/java/possebom/com/teamswidgets/BaseActivity.java
@@ -0,0 +1,29 @@
+package possebom.com.teamswidgets;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by alexandre on 30/10/14.
+ */
+public abstract class BaseActivity extends ActionBarActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResource());
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    protected abstract int getLayoutResource();
+
+    protected void setActionBarIcon(int iconRes) {
+        toolbar.setNavigationIcon(iconRes);
+    }
+}
